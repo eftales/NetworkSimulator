9a82aa90ad8ef20bd2469fc14a3c1431a9cff051
eftales <1479803084@qq.com>
2022-01-03 12:13:58 +0800

    完善了注释

--- src/eventbus.ts
+++ src/eventbus.ts
@@ -51,14 +51,14 @@ export class EventBus{
             let event = this.heap.pop();
 
             if(this.set.has(event.frame.handler)){
-                this.emitter.emit(event.frame.handler+"recv",event);
+                this.emitter.emit(event.frame.handler+"recv",event); // 因为这里是阻塞执行的，所以当返回时交换机一定完成了转发
                 if(event.frame.handler.search(deviceTypes.typeSwitch)){
-                    // 因为交换机有转发时延，为保证时序正确，必须在交换机转发后睡眠一段时间
-                    setTimeout(this.dispatch.bind(this),this.forwardTime+5);
+                    // 因为交换机有转发时延，为保证时序正确，必须在交换机转发后睡眠 forwardTime 这么长的时间
+                    setTimeout(this.dispatch.bind(this),this.forwardTime);
                 }
                 else{
-                    // 终端则可以直接苏醒
-                    setTimeout(this.dispatch.bind(this),0);// 放弃时间片，
+                    // 终端则可以直接苏醒，因为终端发送报文没有时延
+                    setTimeout(this.dispatch.bind(this),0);// 放弃时间片，让交换机进行处理
                 }
         
             }
@@ -68,7 +68,7 @@ export class EventBus{
         }
         else{
             // 堆栈中没有事件，继续等待
-            setTimeout(this.dispatch.bind(this),this.forwardTime+5);
+            setTimeout(this.dispatch.bind(this),this.forwardTime);
         }
 
         
